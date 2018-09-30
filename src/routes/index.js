0319e03c2833e275d6cb9be68df3740e3c0c6f41
Javier Ortiz <javier.ortizsaorin@gmail.com>
2018-09-30 12:15:13 -0400

    [FEAT] Upgrade apollo server

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
-import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import jwt from 'express-jwt';
 import CustomersRest from '../api/customers/rest';
 import Cms from '../api/cms';
@@ -16,31 +15,30 @@ export default ({ app, container }) => {
   const graphqlSchema = container.resolve('graphqlSchema');
   const config = container.resolve('config');
 
+  const apolloServer = new ApolloServer({
+    schema: graphqlSchema,
+    context: ({ req }) => ({
+      customersService,
+      productsService,
+      authUser: req.user,
+    }),
+    tracing: process.env.NODE_ENV === 'production',
+    cacheControl:
+      process.env.NODE_ENV === 'production'
+        ? {
+          defaultMaxAge: config.get('APOLLO:ENGINE:CACHE_DEFAULT_MAX_AGE') || 240,
+        }
+        : false,
+  });
+
   app.use('/api/customers', CustomersRest({ router, customersController, authenticateMiddleware }));
   app.use('/api/cms', Cms({ router, cmsController }));
   app.use('/ping', (req, res) => res.send('I am alive'));
 
-  app.use(
-    '/graphql',
-    jwt({ secret: config.get('TOKEN:SECRET'), credentialsRequired: false }),
-    bodyParser.json(),
-    graphqlExpress(req => ({
-      schema: graphqlSchema,
-      context: {
-        customersService,
-        productsService,
-        authUser: req.user,
-      },
-      tracing: process.env.NODE_ENV === 'production',
-      cacheControl:
-        process.env.NODE_ENV === 'production'
-          ? {
-            defaultMaxAge: config.get('APOLLO:ENGINE:CACHE_DEFAULT_MAX_AGE') || 240,
-          }
-          : false,
-    })),
-  );
-  app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+  app.use('/graphql', jwt({ secret: config.get('TOKEN:SECRET'), credentialsRequired: false }));
+  apolloServer.applyMiddleware({
+    app,
+  });
 
   // All other routes 404
   app.route('/*').get((req, res) => {
