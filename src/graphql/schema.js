fcfbf6f8ad9dad04ad3fb9a7b71f408bc2af3852
Javi Ortiz <javier.ortizsaorin@gmail.com>
2018-09-30 16:48:53 -0400

    Feature/product search delegate schema (#48)
    * [FEAT] Upgrade apollo server
    
    * [FEAT] Delegate the Product type resolution to commercetools
    
    In the product search, the Product type resolution is delegated to commercetools via schema stiching

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,6 +1,51 @@
-import { mergeSchemas } from 'graphql-tools';
+import { mergeSchemas, WrapQuery } from 'graphql-tools';
+import { Kind } from 'graphql';
+
+const productLinkTypeDefs = `
+  extend type ProductsSearchQueryResult {
+    results: [Product!]!
+  }
+`;
 
 export default ({ commercetoolsSchema, customersSchema, productsSchema }) =>
   mergeSchemas({
-    schemas: [customersSchema, commercetoolsSchema, productsSchema],
+    schemas: [customersSchema, commercetoolsSchema, productsSchema, productLinkTypeDefs],
+    resolvers: {
+      ProductsSearchQueryResult: {
+        results: {
+          fragment: '... on ProductsSearchQueryResult { productIds }',
+          resolve(productsSearchQueryResult, args, context, info) {
+            const { productIds } = productsSearchQueryResult;
+            if (!productIds.length) {
+              return [];
+            }
+
+            return info.mergeInfo.delegateToSchema({
+              schema: commercetoolsSchema,
+              operation: 'query',
+              fieldName: 'products',
+              args: {
+                where: `id in (${productIds.map(id => `"${id}"`).join(',')})`,
+              },
+              context,
+              info,
+              transforms: [
+                new WrapQuery(
+                  ['products'],
+                  subtree => ({
+                    kind: Kind.FIELD,
+                    name: {
+                      kind: Kind.NAME,
+                      value: 'results',
+                    },
+                    selectionSet: subtree,
+                  }),
+                  productQueryResult => productQueryResult.results,
+                ),
+              ],
+            });
+          },
+        },
+      },
+    },
   });
